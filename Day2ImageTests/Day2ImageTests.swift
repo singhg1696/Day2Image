ce04871628d594915c807b2242e52e7d6243a5c7
Gurwinder Singh <gurwinder958652@gmail.com>
2019-07-02 18:26:57 -0400

    Initial Commit

---
+++ Day2ImageTests/Day2ImageTests.swift
@@ -0,0 +1,34 @@
+//
+//  Day2ImageTests.swift
+//  Day2ImageTests
+//
+//  Created by MacStudent on 2019-07-02.
+//  Copyright © 2019 MacStudent. All rights reserved.
+//
+
+import XCTest
+@testable import Day2Image
+
+class Day2ImageTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
